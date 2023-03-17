bfe62b80c618596258846f9794517dc3a1ad69c5
samuel ho <samuelho343@gmail.com>
2023-03-16 19:19:12 -0700

    game logic and spawn

--- src/game/Canvas.tsx
+++ src/game/Canvas.tsx
@@ -1,48 +1,88 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import GameLogic from './GameLogic';
+import SpawnLogic from './SpawnLogic';
 
-function Canvas({ handleGameModal }) {
+function Canvas({ handleGameModal, gameModal }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000,
-    );
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+    const scene = createScene();
+    if (gameModal) {
+      const camera = createCamera();
+      const renderer = createRenderer(canvasRef);
+      const light = createLight();
+      scene.add(light);
 
-    camera.position.z = 5;
+      SpawnLogic(scene);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-    const cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
-
-    const color = 0xffffff;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
-    };
-
-    animate();
+      animate(renderer, scene, camera);
+    } else {
+      let obj;
+      for (var i = scene.children.length - 1; i >= 0; i--) {
+        obj = scene.children[i];
+        console.log('here deleting');
+        scene.remove(obj);
+      }
+    }
   }, []);
+
+  const handleRefClick = (e) => {
+    if (e.target !== canvasRef.current) {
+      handleGameModal();
+    }
+  };
+
   return (
-    <div className=" absolute top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-30">
+    <div
+      onClick={handleRefClick}
+      className="absolute top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-30"
+    >
       <canvas ref={canvasRef}></canvas>
     </div>
   );
+}
+
+function createScene() {
+  return new THREE.Scene();
+}
+
+function createCamera() {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+  );
+  camera.position.z = 5;
+  return camera;
+}
+
+function createRenderer(canvasRef) {
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvasRef.current,
+    alpha: true,
+  });
+  renderer.setClearColor(0x000000, 0);
+  renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
+  return renderer;
+}
+
+function createLight() {
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  return light;
+}
+
+function animate(renderer, scene, camera) {
+  const loop = () => {
+    requestAnimationFrame(loop);
+    renderer.render(scene, camera);
+  };
+
+  loop();
 }
 
 export default Canvas;
---
+++ src/game/SpawnLogic.tsx
@@ -0,0 +1,72 @@
+import { gsap } from 'gsap';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+
+function SpawnLogic(scene) {
+  loadGLTFModel().then((object) => {
+    const animatedObj = Position(object);
+    animatedObj.customRotationX = Math.PI * Math.random();
+    animatedObj.customRotationY = Math.PI * Math.random();
+    animatedObj.customRotationZ = Math.PI * Math.random();
+    scene.add(animatedObj);
+    AnimateFall(animatedObj);
+
+    const randomInterval = Math.random() * 4;
+    setTimeout(() => SpawnLogic(scene), randomInterval * 100);
+  });
+}
+
+export default SpawnLogic;
+
+function loadGLTFModel() {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader();
+    loader.load(
+      '/DogeCoin.gltf',
+      (gltf) => {
+        const object = gltf.scene;
+        resolve(object);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+      },
+      (error) => {
+        console.error('An error occurred while loading the glTF model:', error);
+        reject(error);
+      },
+    );
+  });
+}
+
+function Position(object) {
+  const scale = 0.05;
+  object.position.x = Math.random() * 10;
+  object.position.y = 10;
+  object.scale.set(scale, scale, scale);
+  return object;
+}
+
+function AnimateFall(object) {
+  gsap.to(object.position, {
+    duration: 5,
+    y: -10,
+    ease: 'power1.in',
+    onComplete: () => {
+      object.dispose();
+    },
+  });
+
+  animateRotation(object.rotation, 'x', object.customRotationX);
+  animateRotation(object.rotation, 'y', object.customRotationY);
+  animateRotation(object.rotation, 'z', object.customRotationZ);
+}
+
+function animateRotation(rotation, axis, customRotation) {
+  const initialRotation = rotation[axis];
+  gsap.to(rotation, {
+    duration: 0.3,
+    [axis]: initialRotation + customRotation,
+    repeat: -1,
+    ease: 'power1',
+  });
+}
