ed3616875ae0c06ca3830bd9a9f84dc0ea324123
samuel ho <samuelho343@gmail.com>
2023-03-20 15:51:08 -0700

    workhistory animation

--- src/pages/work-history.tsx
+++ src/pages/work-history.tsx
@@ -14,66 +14,70 @@ function WorkHistory() {
     setDetail(entry);
   };
 
-  const animateDetail = (opacity: number, width: string) => {
+  const animateDetail = (opacity: number, width: string, height: string) => {
     if (detailRef.current) {
       gsap.to(detailRef.current, {
-        duration: 0.2,
+        duration: 0.5,
         opacity,
         width,
-        ease: 'power1.out',
+        height,
+        ease: 'power2.out',
       });
     }
   };
 
   const animateContainer = (width: string) => {
-    gsap.to(containerRef.current, { duration: 0.5, width, ease: 'power1.out' });
+    gsap.to(containerRef.current, { duration: 0.7, width, ease: 'power1.out' });
   };
 
   return (
-    <div className="mx-30 flex rounded-xl bg-customWhite">
+    <div
+      className="mx-30 flex max-h-full rounded-xl bg-customWhite"
+      onMouseOver={(e) => {
+        animateContainer('65%');
+      }}
+      onMouseLeave={(e) => {
+        setDetail(null);
+        animateContainer('80%');
+        animateDetail(0, '30%', '0');
+      }}
+    >
       <div
         ref={containerRef}
-        onMouseOver={(e) => {
-          animateContainer('50%');
-          animateDetail(1, '100%');
-        }}
-        onMouseLeave={() => {
-          setDetail(null);
-          animateContainer('75%');
-          animateDetail(0, '0%');
-        }}
-        className="flex w-full flex-col gap-6 rounded-xl bg-customOrange p-6 "
+        className="flex w-5/6 flex-col gap-6 rounded-xl bg-customOrange p-6 "
       >
         <h1>Previously..</h1>
         {experience.map((entry, i) => (
           <div
             key={i}
-            onMouseOver={(e) => handleDetails(e, entry)}
+            onMouseOver={(e) => {
+              handleDetails(e, entry);
+              animateContainer('40%');
+              animateDetail(1, '100%', 'auto');
+            }}
             onClick={(e) => handleDetails(e, entry)}
             className="min-w-2/3 flex justify-between rounded-xl bg-customOrange p-6 outline-dotted hover:outline-double"
           >
-            <p>{entry.employer}</p>
+            {entry.employer}
             {!detail && <p>{entry.title}</p>}
           </div>
         ))}
       </div>
-      <div
-        ref={detailRef}
-        className="translate-y-30 h-full p-6 opacity-0 transition-all duration-200"
-      >
-        {detail && (
-          <>
-            <p>{detail.location}</p>
-            <p>{detail.dates}</p>
+      {detail && (
+        <div
+          ref={detailRef}
+          className="translate-y-30 h-0 overflow-y-hidden p-6 opacity-0 transition-all duration-200"
+        >
+          <p>{detail.location}</p>
+          <p>{detail.dates}</p>
 
-            {detail.responsibilities.map((el, i) => (
-              <div key={i}>
-                <p>{el}</p>
-              </div>
-            ))}
-          </>
-        )}
-      </div>
+          {detail.responsibilities.map((el, i) => (
+            <div key={i}>
+              <p>{el}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
