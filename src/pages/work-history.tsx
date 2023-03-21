92037da5f5f90a5e59788f68f4bc1a7443d03388
samuel ho <samuelho343@gmail.com>
2023-03-20 22:53:31 -0700

    animation tweak

--- src/pages/work-history.tsx
+++ src/pages/work-history.tsx
@@ -10,74 +10,74 @@ function WorkHistory() {
 
   const handleDetails = (e, entry) => {
     e.preventDefault();
-    console.log(entry);
     setDetail(entry);
   };
 
-  const animateDetail = (opacity: number, width: string, height: string) => {
+  const animateTitles = (width: string) => {
+    gsap.to(containerRef.current, { duration: 0.7, width, ease: 'power1.out' });
+  };
+
+  const animateDescription = (opacity: number, width: string) => {
     if (detailRef.current) {
       gsap.to(detailRef.current, {
-        duration: 0.5,
+        duration: 0.2,
         opacity,
         width,
-        height,
         ease: 'power2.out',
       });
     }
   };
 
-  const animateContainer = (width: string) => {
-    gsap.to(containerRef.current, { duration: 0.7, width, ease: 'power1.out' });
-  };
-
   return (
-    <div
-      className="mx-30 flex max-h-full rounded-xl bg-customWhite"
-      onMouseOver={(e) => {
-        animateContainer('65%');
-      }}
-      onMouseLeave={(e) => {
-        setDetail(null);
-        animateContainer('80%');
-        animateDetail(0, '30%', '0');
-      }}
-    >
+    <div className="flex h-1/2 items-center justify-center">
       <div
-        ref={containerRef}
-        className="flex w-5/6 flex-col gap-6 rounded-xl bg-customOrange p-6 "
+        className="mx-8 flex w-full rounded-xl bg-customWhite"
+        onMouseOver={(e) => {
+          animateTitles('90%');
+        }}
+        onMouseLeave={(e) => {
+          // setDetail(null);
+          animateTitles('100%');
+        }}
       >
-        <h1>Previously..</h1>
-        {experience.map((entry, i) => (
-          <div
-            key={i}
-            onMouseOver={(e) => {
-              handleDetails(e, entry);
-              animateContainer('40%');
-              animateDetail(1, '100%', 'auto');
-            }}
-            onClick={(e) => handleDetails(e, entry)}
-            className="min-w-2/3 flex justify-between rounded-xl bg-customOrange p-6 outline-dotted hover:outline-double"
-          >
-            {entry.employer}
-            {!detail && <p>{entry.title}</p>}
-          </div>
-        ))}
-      </div>
-      {detail && (
         <div
-          ref={detailRef}
-          className="translate-y-30 h-0 overflow-y-hidden p-6 opacity-0 transition-all duration-200"
+          ref={containerRef}
+          className="mr-8 flex flex-col gap-6 rounded-xl bg-customOrange p-6"
         >
-          <p>{detail.location}</p>
-          <p>{detail.dates}</p>
-
-          {detail.responsibilities.map((el, i) => (
-            <div key={i}>
-              <p>{el}</p>
+          <h1>Previously..</h1>
+          {experience.map((entry, i) => (
+            <div
+              key={i}
+              onMouseOver={(e) => {
+                animateTitles('80%');
+              }}
+              onClick={(e) => {
+                handleDetails(e, entry);
+                animateDescription(1, '100%');
+              }}
+              className="flex h-full justify-between rounded-xl bg-customOrange p-6 outline-dotted hover:outline-double"
+            >
+              {entry.employer}
+              {!detail && <p>{entry.title}</p>}
             </div>
           ))}
         </div>
-      )}
+
+        {detail && (
+          <div
+            ref={detailRef}
+            className="overflow-y-hidden p-6 transition-all duration-200"
+          >
+            <p>{detail.location}</p>
+            <p>
+              <ins>{detail.dates}</ins>
+            </p>
+            {detail.responsibilities.map((el, i) => (
+              <p key={i}>- {el}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
