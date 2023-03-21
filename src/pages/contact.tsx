0ad293761a44cf7ab1f31bd3cbc9c3e9d06ad47f
samuel ho <samuelho343@gmail.com>
2023-03-20 22:53:41 -0700

    mail api

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -50,63 +50,52 @@ function Contact() {
   };
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="flex h-full  flex-col items-center gap-20 rounded-3xl bg-customYellow p-4">
+      <div className="flex h-full w-1/2 flex-col items-center gap-20 rounded-3xl bg-customYellow p-4">
+        <h1 className="max-w-fit rounded-3xl bg-customBlue px-4">Contact Me</h1>
         {!submit ? (
-          <>
-            <h1 className="max-w-fit rounded-3xl bg-customBlue px-4">
-              Contact Me
-            </h1>
+          <div className="w-1/2">
             <form className="flex w-full flex-col justify-center gap-2">
-              <label
-                htmlFor="name"
-                className="mt-8 font-light text-gray-500 dark:text-gray-50"
-              >
+              <label htmlFor="name" className="mt-8 font-light text-black">
                 Full name
-                <span className="text-red-500 dark:text-gray-50">*</span>
+                <span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
                 name="name"
                 onChange={handleForm}
-                className="border-b bg-transparent py-2 pl-4 font-light text-gray-500 ring-green-500 focus:rounded-md focus:outline-none focus:ring-1"
+                className="border-b bg-transparent py-2 pl-4 font-light text-black ring-customOrange focus:rounded-md focus:outline-none focus:ring-1"
               />
 
-              <label
-                htmlFor="email"
-                className="mt-4 font-light text-gray-500 dark:text-gray-50"
-              >
+              <label htmlFor="email" className="mt-4 font-light text-black">
                 E-mail<span className="text-red-500">*</span>
               </label>
               <input
                 type="email"
                 name="email"
                 onChange={handleForm}
-                className="border-b bg-transparent py-2 pl-4 font-light text-gray-500 ring-green-500 focus:rounded-md focus:outline-none focus:ring-1"
+                className="border-b bg-transparent py-2 pl-4 font-light text-black ring-customOrange focus:rounded-md focus:outline-none focus:ring-1"
               />
 
-              <label
-                htmlFor="subject"
-                className="mt-4 font-light text-gray-500 dark:text-gray-50"
-              >
+              <label htmlFor="subject" className="mt-4 font-light text-black">
                 Subject<span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
                 name="subject"
                 onChange={handleForm}
-                className="border-b bg-transparent py-2 pl-4 font-light text-gray-500 ring-green-500 focus:rounded-md focus:outline-none focus:ring-1"
+                className="border-b bg-transparent py-2 pl-4 font-light text-black ring-customOrange focus:rounded-md focus:outline-none focus:ring-1"
               />
 
               <label
                 htmlFor="message"
                 onChange={handleForm}
-                className="mt-4 font-light text-gray-500 dark:text-gray-50"
+                className="mt-4 font-light text-black"
               >
                 Message<span className="text-red-500">*</span>
               </label>
               <textarea
                 name="message"
-                className="border-b bg-transparent py-2 pl-4 font-light text-gray-500 ring-green-500 focus:rounded-md focus:outline-none focus:ring-1"
+                className="border-b bg-transparent py-2 pl-4 font-light text-black ring-customOrange focus:rounded-md focus:outline-none focus:ring-1"
                 onChange={handleForm}
               ></textarea>
               <div className="flex justify-end ">
@@ -119,7 +108,7 @@ function Contact() {
                 </button>
               </div>
             </form>
-          </>
+          </div>
         ) : (
           <div>Submitted</div>
         )}
