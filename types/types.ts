5b8cd959763b1b905d14e4bcf9fc6358fa49ce88
samuel ho <samuelho343@gmail.com>
2023-03-21 20:23:58 -0700

    types

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,23 @@
-export default interface state {
-  handleRoute: Function;
+import { MouseEventHandler } from 'react';
+
+interface Routes {
+  handleRoute: (curPage: string) => void;
+  handleGameModal: MouseEventHandler<HTMLButtonElement>;
+  gameModal: boolean;
+}
+export default Routes;
+export interface Experience {
+  employer: string;
+  employer_url: string;
+  location: string;
+  title: string;
+  dates: string;
+  responsibilities: string[];
+}
+
+export interface Form {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 }
