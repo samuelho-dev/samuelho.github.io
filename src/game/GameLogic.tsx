8ff5d40907517490d0a1f72f24afd2fe58232036
samuel ho <samuelho343@gmail.com>
2023-03-21 20:42:00 -0700

    temp types

--- src/game/GameLogic.tsx
+++ src/game/GameLogic.tsx
@@ -1,9 +1,9 @@
 import SpawnLogic from './SpawnLogic';
 
-function GameLogic(player, item, scene) {
-  SpawnLogic(item, scene);
+function GameLogic(player: any, item: any, scene: any) {
+  SpawnLogic(scene);
   const randomInterval = Math.random() * 4;
-  setTimeout(() => SpawnLogic(item, scene), randomInterval * 1000);
+  // setTimeout(() => SpawnLogic(item, scene), randomInterval * 1000);
 }
 
 export default GameLogic;
