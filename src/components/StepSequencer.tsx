8c4a37ae3153551b55122ed4c0598fca501abb75
samuel ho <samuelho343@gmail.com>
2023-03-22 16:20:53 -0700

    working stepsequence

--- src/components/StepSequencer.tsx
+++ src/components/StepSequencer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Sampler } from 'tone';
+import * as Tone from 'tone';
+import { Sampler, Transport } from 'tone';
 import hiHat from 'public/sounds/NAMI_Hi-Hat_Armani.wav';
 import Snare from 'public/sounds/RL_-_Razor_Snare.wav';
 import Kick from 'public/sounds/Slappy_Kick_35.wav';
@@ -8,64 +9,88 @@ import Bongo from 'public/sounds/SD3_BONGO33.wav';
 const notes = [hiHat, Snare, Kick, Bongo];
 
 const makeGrid = (notes: string[]) => {
-  const rows = [];
-
-  for (const note of notes) {
-    const row = [];
-
-    for (let i = 0; i < 8; i++) {
-      row.push({
-        note: note,
+  return notes.map((note, i) => {
+    return Array.from({ length: 8 }, () => {
+      return {
+        key: `C${3 + i}`,
+        note,
         isActive: false,
-      });
-    }
-    rows.push(row);
-  }
-
-  return rows;
+      };
+    });
+  });
 };
-
 function StepSequencer() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [grid, setGrid] = useState(makeGrid(notes));
+  const [loaded, setLoaded] = useState(false);
+  const [play, setPlay] = useState(false);
+  const [tempo, setTempo] = useState(120);
   const stepSequencer = useRef(null);
-
+  const [grid, setGrid] = useState(makeGrid(notes));
   useEffect(() => {
     stepSequencer.current = new Sampler(
-      { C3: hiHat },
+      { C3: hiHat, C4: Snare, C5: Kick, C6: Bongo },
       {
-        onload: () => setIsLoaded(true),
+        onload: () => setLoaded(true),
+        onerror: () => setLoaded(false),
       },
     ).toDestination();
-  });
+  }, []);
+
+  let beat = 0;
+  const handleLoop = () => {
+    const repeat = (time) => {
+      grid.forEach((row) => {
+        const note = row[beat];
+        if (note.isActive) {
+          stepSequencer.current.triggerAttackRelease(note.key, '8n', time);
+        }
+      });
+      beat = (beat + 1) % 8;
+    };
+
+    Transport.bpm.value = tempo;
+    Transport.scheduleRepeat(repeat, '8n');
+  };
 
   const handlePlay = () => {
-    stepSequencer.current.triggerAttackRelease('C3', '8n');
+    if (loaded) {
+      Tone.start();
+      Tone.getDestination().volume.rampTo(-10, 0.0001);
+      setLoaded(true);
+    }
+
+    if (play) {
+      Tone.Transport.stop();
+      setPlay(false);
+    } else {
+      Tone.Transport.start();
+      setPlay(true);
+      handleLoop();
+    }
   };
 
-  const handleNoteClick = (colIdx: number, rowIdx: number) => {
+  const handleNoteClick = (rowIdx: number, noteIdx: number) => {
     const copyGrid = [...grid];
-    copyGrid[colIdx][rowIdx].isActive = !copyGrid[colIdx][rowIdx].isActive;
+    copyGrid[rowIdx][noteIdx].isActive = !copyGrid[rowIdx][noteIdx].isActive;
     setGrid(copyGrid);
   };
+
   return (
     <div>
-      <button disabled={!isLoaded} onClick={handlePlay}>
-        Cmon
-      </button>
+      <button onClick={handlePlay}>{!play ? 'Play' : 'Pause'}</button>
       <h1>StepSequencer</h1>
-      {grid.map((col, colIdx) => (
-        <div key={colIdx} className="flex gap-6 py-2">
-          {col.map((note, rowIdx) => (
+      {grid.map((col, rowIdx) => (
+        <div key={rowIdx} className="flex gap-6 py-2">
+          {col.map((note, noteIdx) => (
             <button
+              disabled={!loaded}
               className={`h-8 w-8  ${
                 note.isActive
                   ? 'rounded-lg bg-customGreen'
                   : 'rounded-md bg-customWhite'
               }`}
-              onClick={() => handleNoteClick(colIdx, rowIdx)}
-              key={`note-${colIdx}-${rowIdx}`}
-            ></button>
+              onClick={() => handleNoteClick(rowIdx, noteIdx)}
+              key={`note-${rowIdx}-${noteIdx}`}
+            />
           ))}
         </div>
       ))}
