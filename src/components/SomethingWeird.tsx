dc0368d31dac1e9a9b55ff4bde010ad355c40042
samuel ho <samuelho343@gmail.com>
2023-03-22 19:58:57 -0700

    googly eyes

--- src/components/SomethingWeird.tsx
+++ src/components/SomethingWeird.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function SomethingWeird() {
-  return <div className="h-1/4 bg-customWhite">SomethingWeird</div>;
+  const eyesRef = useRef(null);
+  const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
+
+  const handleEyesMove = () => {
+    if (eyesRef.current) {
+      const rect = eyesRef.current.getBoundingClientRect();
+      const mouseX = mouseCoordinates.x - rect.left - rect.width / 2;
+      const mouseY = mouseCoordinates.y - rect.top - rect.height / 2;
+
+      const offsetX = Math.max(0, Math.min(mouseX, rect.width));
+      const offsetY = Math.max(0, Math.min(mouseY, rect.height));
+
+      return { left: offsetX, top: offsetY };
+    }
+  };
+  const handleMouseMove = (e: MouseEvent) => {
+    setMouseCoordinates({ x: e.clientX, y: e.clientY });
+    handleEyesMove();
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className="flex h-fit w-fit items-center gap-4">
+      <div className="relative h-10 w-6 rounded-full bg-white">
+        <div
+          ref={eyesRef}
+          className="relative h-4 w-4 rounded-full bg-black"
+          style={handleEyesMove()}
+        />
+      </div>
+      <div className=" h-10 w-6 rounded-full bg-white">
+        <div
+          className="relative h-4 w-4 rounded-full bg-black"
+          style={handleEyesMove()}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SomethingWeird;
