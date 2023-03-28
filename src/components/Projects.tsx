da647a31d1a8c673b960d743340d0c75c4101cdc
samuel ho <samuelho343@gmail.com>
2023-03-27 21:37:00 -0700

    added other projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,13 +3,21 @@ import data from '../../util/resume.json';
 import { TbBrandNextjs } from 'react-icons/tb';
 import { FaNodeJs, FaReact } from 'react-icons/fa';
 import { FaAws } from 'react-icons/fa';
-import { SiFirebase, SiTypescript } from 'react-icons/si';
+import {
+  SiFirebase,
+  SiFlask,
+  SiOpenai,
+  SiPython,
+  SiReact,
+  SiTypescript,
+} from 'react-icons/si';
 import { SiPrisma } from 'react-icons/si';
 import { SiPostgresql } from 'react-icons/si';
 import { SiReactquery } from 'react-icons/si';
 import { SiJavascript } from 'react-icons/si';
 import { SiSequelize } from 'react-icons/si';
 import { BsGithub } from 'react-icons/bs';
+import { HiOutlinePlusCircle } from 'react-icons/hi';
 import Image from 'next/image';
 
 interface IconMap {
@@ -17,7 +25,8 @@ interface IconMap {
 }
 
 function Projects() {
-  const projects = data.projects;
+  const featuredProjects = data.featuredProjects;
+  const otherProjects = data.otherProjects;
   const iconmap: IconMap = {
     'React Native': <FaReact color="white" />,
     'React Query': <SiReactquery color="white" />,
@@ -27,6 +36,11 @@ function Projects() {
     PostgreSQL: <SiPostgresql color="white" />,
     Javascript: <SiJavascript color="white" />,
     Sequelize: <SiSequelize color="white" />,
+    'Open AI': <SiOpenai color="white" />,
+    Flask: <SiFlask color="white" />,
+    Python: <SiPython color="white" />,
+    React: <SiReact color="white" />,
+    Plus: <HiOutlinePlusCircle color="white" />,
     AWS: <FaAws color="white" />,
     Node: <FaNodeJs color="white" />,
     NextJS: <TbBrandNextjs color="white" />,
@@ -35,7 +49,7 @@ function Projects() {
     <section className="max-w-6xl">
       <h2 className=" text-slate-200">Projects</h2>
       <ul className="flex flex-col items-center gap-24 md:gap-4">
-        {projects.map((project, i) => (
+        {featuredProjects.map((project, i) => (
           <li
             key={i}
             className={`flex w-fit flex-col gap-4  md:flex-row md:p-6 ${
@@ -112,6 +126,37 @@ function Projects() {
           </li>
         ))}
       </ul>
+      <div className="flex flex-col gap-2">
+        <h5>Other Projects</h5>
+        <div className="flex">
+          {otherProjects.map((project, i) => (
+            <a
+              key={i}
+              href={project.source}
+              target="_blank"
+              className="flex w-full"
+            >
+              <div className="m-2 flex cursor-pointer flex-col justify-between bg-customOrange bg-opacity-70 p-2">
+                <div className="flex items-center gap-2">
+                  <div>{iconmap.Plus}</div>
+                  <h5 className="w-fit border-b-2 border-transparent  transition duration-300 hover:border-customRed">
+                    {project.name}
+                  </h5>
+                </div>
+                <p>{project.description}</p>
+                <div className="flex gap-4">
+                  {project.technologies.map((tech, j) => (
+                    <div key={j} className={`flex items-center gap-2 pt-4`}>
+                      <div>{iconmap[tech]}</div>
+                      <sub className="text-customWhite">{tech}</sub>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
