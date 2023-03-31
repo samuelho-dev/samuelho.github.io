37148239e2356126ae5cef1bd391bd93ab847dab
samuel ho <samuelho343@gmail.com>
2023-03-31 11:46:47 -0700

    env update

--- util/nodemailer.ts
+++ util/nodemailer.ts
@@ -7,8 +7,8 @@ export const transporter = nodemailer.createTransport({
   service: 'gmail',
 
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
+    user: email,
+    pass,
   },
 });
 
