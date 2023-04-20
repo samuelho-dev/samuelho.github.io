e9c32fc14c23611fb7ffacd34bfed7bd7966c7d9
samuelho-dev <samuelho343@gmail.com>
2023-04-20 00:43:14 -0700

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '@/layout/Layout';
 import { useRouter } from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -18,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Analytics />
       <div className="flex min-h-screen flex-col justify-between gap-10 bg-zinc-800 px-0 lg:py-5 lg:px-28">
         <Layout handleRoute={handleRoute}>
           <Component {...pageProps} handleRoute={handleRoute} />
