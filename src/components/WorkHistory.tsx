d2ae77e93d68df962d51707b46cc84e24617bd83
Samuel Ho <samuelho343@gmail.com>
2023-07-24 22:48:14 -0700

    styling work history

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -20,7 +20,7 @@ function WorkHistory() {
   };
 
   return (
-    <section className="flex h-1/2 max-w-5xl flex-col justify-center gap-4 py-0 md:py-16">
+    <section className="flex flex-col justify-center gap-4 py-0 md:py-16">
       <h2 className="text-slate-200">Work History</h2>
       <div className="tileShadow flex flex-col rounded-xl bg-customWhite outline outline-1 md:flex-row">
         <div
@@ -28,7 +28,7 @@ function WorkHistory() {
           className="flex w-full flex-col gap-6 rounded-xl bg-customOrange p-4 md:w-1/2 md:p-6"
         >
           <div className="flex items-center justify-between">
-            <h2>Previously..</h2>
+            <h2 className="text-lg font-extrabold">Previously..</h2>
             <Link
               href={'/Resume_SamuelHo.docx'}
               download
@@ -47,10 +47,10 @@ function WorkHistory() {
                 onClick={(e) => {
                   handleDetails(e, entry);
                 }}
-                className={`tileShadow flex cursor-pointer justify-between rounded-xl  p-2 outline-dotted hover:outline-double md:p-6 ${
+                className={`tileShadow flex cursor-pointer justify-between rounded-xl p-2 text-2xl outline-dotted hover:outline-double md:p-6 ${
                   detail.employer === entry.employer
-                    ? 'bg-customGreen outline-double'
-                    : 'bg-orange-400 bg-opacity-50'
+                    ? 'bg-customBlack outline-double'
+                    : 'bg-orange-500'
                 }`}
               >
                 {entry.employer}
@@ -58,33 +58,33 @@ function WorkHistory() {
             ))}
           </div>
         </div>
-        <div ref={detailRef} className="w-full p-6 md:w-1/2">
-          <div className="flex flex-col items-start justify-between md:flex-row md:items-center">
-            <div className="flex flex-wrap items-center gap-1">
-              <h2 className="text-black">{detail.title}</h2>
+        <div ref={detailRef} className="w-full p-2 md:w-1/2">
+          <div className="flex h-24 flex-col items-start justify-between rounded-lg bg-black bg-opacity-10 p-2 md:flex-row md:items-center">
+            <div className="flex w-full flex-wrap items-center gap-1">
+              <h2 className="font-semibold text-customBlack">{detail.title}</h2>
               <div className="flex cursor-pointer items-center gap-1">
-                <h3 className="text-customBlue">@</h3>
+                <h3 className="text-orange-500">@</h3>
                 <a href={detail.employer_url} target="_blank">
-                  <h3 className="border-b-2 border-transparent text-customBlue transition duration-300 hover:border-customBlue">
+                  <h3 className="border-b-2 border-transparent font-semibold text-orange-500 transition duration-300 hover:border-orange-500">
                     {detail.employer}
                   </h3>
                 </a>
               </div>
             </div>
-            <div className="flex w-full items-center justify-between md:w-fit md:flex-col  ">
-              <p>{detail.location}</p>
-              <p className="whitespace-nowrap py-2 text-xs tracking-wide">
+            <div className="flex w-full items-end justify-between md:flex-col">
+              <p className="font-bold text-customBlack">{detail.location}</p>
+              <p className="whitespace-nowrap text-xs font-bold tracking-wide text-customBlack">
                 {detail.dates}
               </p>
             </div>
           </div>
-          <div className="flex  flex-col items-center justify-center gap-4 pt-2">
+          <div className="flex flex-col justify-center gap-4 p-2">
             {detail.responsibilities.map((el, i) => (
-              <p key={i} className="flex items-start">
-                <span className="h-4 w-4 py-2 text-slate-700">
+              <p key={i} className="flex items-start justify-start gap-2">
+                <span className="h-4 w-4 bg-orange-500">
                   <TbArrowBadgeRight />
                 </span>
-                <span className="text-slate-700">{el}</span>
+                <span className="font-mono text-customBlack">{el}</span>
               </p>
             ))}
           </div>
