b09757a18a4ce26ea712a54c6163ca90b664d826
Samuel Ho <samuelho343@gmail.com>
2023-07-26 23:31:43 -0700

    add page ref

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,16 @@ import Head from 'next/head';
 import Layout from '@/layout/Layout';
 import { useRouter } from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
+import usePageRefs from 'util/pageRef';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const handleRoute = (curPage: string) => {
     router.push(`/${curPage}`, undefined, { shallow: true });
   };
+
+  const { pageRef, frontpageRef, aboutRef, workRef, contactRef, beatRef } =
+    usePageRefs();
 
   return (
     <>
@@ -21,8 +25,25 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <Analytics />
       <div className="flex min-h-screen w-full items-center justify-between bg-[#252323] px-0 lg:py-5 lg:px-20">
-        <Layout handleRoute={handleRoute}>
-          <Component {...pageProps} handleRoute={handleRoute} />
+        <Layout
+          handleRoute={handleRoute}
+          pageRef={pageRef}
+          frontpageRef={frontpageRef}
+          workRef={workRef}
+          aboutRef={aboutRef}
+          contactRef={contactRef}
+          beatRef={beatRef}
+        >
+          <Component
+            {...pageProps}
+            handleRoute={handleRoute}
+            pageRef={pageRef}
+            frontpageRef={frontpageRef}
+            workRef={workRef}
+            aboutRef={aboutRef}
+            contactRef={contactRef}
+            beatRef={beatRef}
+          />
         </Layout>
       </div>
     </>
